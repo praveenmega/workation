88f4092c3204ea9f4a3bd9a7be6424f3289215d5
Praveen <praveenmeka06@gmail.com>
2021-02-21 11:44:28 +0530

    update website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.jsx", "./index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
+        },
+      },
+      fontFamily: {
+        headline: ["Poppins", "sans-serif"],
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+    },
   },
   plugins: [],
-}
+};
